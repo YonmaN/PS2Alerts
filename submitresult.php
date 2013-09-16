c608183345058a4ab4a8df824ad495ecb68c37f1
Matthew Cavanagh <maelstrome26@gmail.com>
2013-09-16 10:36:52 +0100

    * Added core HTML functionality code (layout / formatting). #2 #1 * Added MySQL Connect script (omitted for security) * Added basic validation for "Cross Continant" disabling Territory Capture when selected #3

--- submitresult.php
+++ submitresult.php
@@ -2,6 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <link href="css/sitewide.css" rel="stylesheet" />
+<?php include("includes/mysqlconnect.php") ?>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Planetside 2 Statistics Index</title>
 </head>
@@ -14,8 +15,83 @@
     <a href="stats.php">Statistics</a>
     </div>
     
+    <?php
+	// Clean all variables at beginning of form
+	
+	$ResultServer = '1'; //Currently always set to Miller
+	$ResultWinner = '';
+	$ResultDraw = '';
+	$ResultAlertCont = '';
+	$ResultAlertType = '';
+	$ResultFacilitiesWon = '';
+	$ResultPopMajority = '';
+	$ResultPopNC = '';
+	$ResultPopTR = '';
+	$ResultPopVS = '';	
+	?>
     <div id="content">
+    <form action="submitresult_facilities.php" method="POST" name="AlertStats">
     
+    <p class="form_headers">General Alert Stats (Miller Only!)</p>    
+    <p class="form_item_text">When did the Alert end? (GMT time):</p>
+    <input class="form_item" type="datetime-local" name="AlertStats[ResultDateTime]" />
+    
+    <p class="form_item_text">Who won this alert?</p>
+    
+    <input type="radio" name="AlertStats[ResultWinner]" value="NC" />New Conglomerate <br />
+    <input type="radio" name="AlertStats[ResultWinner]" value="TR" />Terran Republic <br />
+    <input type="radio" name="AlertStats[ResultWinner]" value="VS" />Vanu Soverignity <br />
+    <input type="radio" name="AlertStats[ResultWinner]" value="Draw" />Draw <br />
+    
+    <p class="form_item_text">Where was this continant based?</p>
+    
+    <input type="radio" name="AlertStats[ResultAlertCont]" value="1" onclick="reenableIfNotCross()"/>Amerish <br />
+    <input type="radio" name="AlertStats[ResultAlertCont]" value="2" onclick="reenableIfNotCross()" />Esamir <br />
+    <input type="radio" name="AlertStats[ResultAlertCont]" value="3" onclick="reenableIfNotCross()" />Indar <br />
+    <input type="radio" name="AlertStats[ResultAlertCont]" value="4" onclick="disableIfCross()"/>Cross Continent (All three) <br />
+    
+    <script>
+	function disableIfCross()
+	{
+		document.getElementById("TypeTerritory").disabled=true
+	}
+	function reenableIfNotCross()
+	{
+		document.getElementById("TypeTerritory").disabled=false
+	}
+	</script>
+    
+    <p class="form_item_text">What kind of alert was this?</p>
+    
+    <!-- Based on the continant answer and this answer, PHP will change the submitted variable to be the proper one into the database -->
+    
+    <input type="radio" name="AlertStats[ResultAlertType]" value="Amp" />Amp Stations <br />
+    <input type="radio" name="AlertStats[ResultAlertType]" value="Bio" />Bio Labs<br />
+    <input type="radio" name="AlertStats[ResultAlertType]" value="Tech" />Tech Plants <br />
+    <input type="radio" name="AlertStats[ResultAlertType]" value="Territory" id="TypeTerritory" />Territory Capture <br />   
+    
+    <br />
+    <input type="submit" name="AlertStats" value="Continue..." />
+    
+   	</form>
+    <p class="form_headers">Population Statistics</p>   
+    
+    
+    <p class="form_item_text">Which faction had the majority of population at the <b>end</b> of the alert?</p>
+    
+    <input type="radio" name="ResultMajorityPop" value="NC" />New Conglomerate <br />
+    <input type="radio" name="ResultMajorityPop" value="TR" />Terran Republic <br />
+    <input type="radio" name="ResultMajorityPop" value="VS" />Vanu Soverignity <br />
+    <input type="radio" name="ResultMajorityPop" value="Even" />Populations were exactly even (33% each) <br />
+    
+    
+
+	<p class="form_item_text">How much population percentage (%) did each of the factions have?</p>
+    New Conglomerate = <input class="two" type="text" name="ResultPopsNC" />% <br />
+    Terran Republic = <input class="two" type="text" name="ResultPopsTR" />% <br />
+    Vanu Soverignty = <input class="two" type="text" name="ResultPopsVS" />% <br />
+
+
 
 
 
---
+++ submitresult_facilities.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<link href="css/sitewide.css" rel="stylesheet" />
+<?php include("includes/mysqlconnect.php") ?>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Planetside 2 Statistics Index</title>
+</head>
+
+<body>
+<div id="wrapper">
+
+	<div id="header">
+    <a href="submitresult.php">Submit Result</a>
+    <a href="stats.php">Statistics</a>
+    </div>
+    
+    <div id="content">
+    <?php
+	$ResultDate = $_POST["ResultDateTime"];
+    $ResultServer = $_POST["ResultServer"]; //Currently always set to Miller
+	$ResultWinner = $_POST["ResultWinner"];
+	$ResultDraw = $_POST["ResultDraw"];
+	$ResultAlertCont = $_POST["ResultAlertCont"];
+	$ResultAlertType = $_POST["ResultAlertType"];
+	$ResultFacilitiesWon = $_POST["ResultFacilitiesWon"];
+	$ResultPopMajority = $_POST["ResultPopMajority"];
+	$ResultPopNC = $_POST["ResultPopNC"];
+	$ResultPopTR = $_POST["ResultPopTR"];
+	$ResultPopVS = $_POST["ResultPopVS"];
+	
+	
+	?>
+    
+    <form action="" method="post" name="AlertStats">
+    
+    <p class="form_headers">Facility Statistics</p>  
+    
+    <p class="form_item_text">Which facility was the most contested?</p>
+    
+    <?php
+	
+	if(isset($_POST['myform'])) {
+    $values = $_POST['AlertStats'];
+	}
+	
+	$ResultAlertType = $_POST("ResultAlertType");
+	$ResultAlertCont = $_POST("ResultAlertCont");
+		
+	$SelectQuery = ("SELECT * FROM facilities WHERE FacilityType ='".$ResultAlertType."' AND FacilityContID = '".$ResultAlertCont."'");	
+	
+	?>
+    
+    <select>
+    	<option value="Allatum">Allatum Bio Lab</option>
+        <option value="Andvari">Andvari Bio Lab</option>
+        <option value="Dahaka">Dahaka Amp Stationb</option>
+        <option value="Allatum">Allatum Bio Lab</option>
+        <option value="Allatum">Allatum Bio Lab</option>
+        <option value="Allatum">Allatum Bio Lab</option>
+        <option value="Allatum">Allatum Bio Lab</option>
+        <option value="Allatum">Allatum Bio Lab</option>
+        <option value="Allatum">Allatum Bio Lab</option>
+        <option value="Allatum">Allatum Bio Lab</option>
+        <option value="Allatum">Allatum Bio Lab</option>
+        <option value="Allatum">Allatum Bio Lab</option>
+        <option value="Allatum">Allatum Bio Lab</option>
+        <option value="Allatum">Allatum Bio Lab</option>
+        <option value="Allatum">Allatum Bio Lab</option>
+        <option value="Allatum">Allatum Bio Lab</option>
+        <option value="Allatum">Allatum Bio Lab</option>
+        <option value="Allatum">Allatum Bio Lab</option>
+        <option value="Allatum">Allatum Bio Lab</option>
+        <option value="Allatum">Allatum Bio Lab</option>
+        <option value="Allatum">Allatum Bio Lab</option>
+        <option value="Allatum">Allatum Bio Lab</option>
+        <option value="Allatum">Allatum Bio Lab</option>
+        <option value="Allatum">Allatum Bio Lab</option>
+        
+    </select>
+
+</form>
+	</div>
+
+
+</div>
+</body>
+</html>
