3f4d3237c326679546e08a1894b62005331852b1
Matthew Cavanagh <maelstrome26@gmail.com>
2013-09-19 10:04:13 +0100

    Updated header & CSS

--- includes/header.php
+++ includes/header.php
@@ -2,8 +2,8 @@
 	<div id="center" style="width: 100%; margin-left: auto; margin-right: auto;">
          <img width="300px" style="display:block; margin-left: 260px;" src="images/Website_Header.png" />
         <div id="links" style="text-align: center; margin-top: 7px;">
-            <a href="submitresult.php">Submit Result</a>
-            <a href="stats.php">Statistics</a>
+            <a class="header_links" href="submitresult.php">Submit Result</a>
+            <a class="header_links" href="stats.php">Statistics</a>
         </div>
     </div>
 </div>
