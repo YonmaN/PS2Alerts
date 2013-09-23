f6b4cdf7c0437a130868843b8bb9df23a6a13cc6
Matthew Cavanagh <maelstrome26@gmail.com>
2013-09-23 11:16:37 +0100

    Added Statistics.php

---
+++ statistics.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<link href="css/sitewide.css" rel="stylesheet" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Planetside 2 Statistics Index</title>
+</head>
+
+<body>
+<div id="wrapper">
+
+	<?php include('includes/header.php') ?>
+    
+    <div id="content">
+    
+	<p class="form_headers">Nothing to see here. Move along.
+	<p class="form_item_text">&quot;These are not the droids you are looking for.&quot;</p>
+	<p class="form_item_text">- Obi Wan Kanobi, Star Wars IV.</p>
+  </div>
+
+
+</div>
+</body>
+</html>
