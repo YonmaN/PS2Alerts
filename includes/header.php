05d84750e079267252b498b2375496007b30cbc7
Matthew Cavanagh <maelstrome26@gmail.com>
2013-09-23 07:19:22 +0100

    Added link to the header image

--- includes/header.php
+++ includes/header.php
@@ -1,9 +1,9 @@
 <div id="header">
 	<div id="center" style="width: 100%; margin-left: auto; margin-right: auto;">
-         <img style="display:block;" src="images/alertbanner.png" />
+        <a href="index.php"><img style="display:block;" src="images/alertbanner.png" /></a>
         <div id="links" style="text-align: center; margin-top: 7px;">
             <a class="header_links" href="submitresult.php">Submit Result</a>
-            <a class="header_links" href="stats.php">Statistics</a>
+            <a class="header_links" href="statistics.php">Statistics</a>
         </div>
     </div>
 </div>
