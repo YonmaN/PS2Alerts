abb022df35c2c5889d923ec15e6996869122805d
Matthew Cavanagh <maelstrome26@gmail.com>
2013-09-23 14:10:12 +0100

    Added credit to Kamteix

--- includes/header.php
+++ includes/header.php
@@ -1,6 +1,6 @@
 <div id="header">
 	<div id="center" style="width: 100%; margin-left: auto; margin-right: auto;">
-        <a href="index.php"><img style="display:block;" src="images/alertbanner.png" /></a>
+        <a href="index.php"><img style="display:block;" src="images/alertbanner.png" alt="Courtesy of Kamteix!"/></a>
         <div id="links" style="text-align: center; margin-top: 7px;">
             <a class="header_links" href="submitresult.php">Submit Result</a>
             <a class="header_links" href="statistics.php">Statistics</a>
