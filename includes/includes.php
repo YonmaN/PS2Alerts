de5989a59072ff7c6b023ef09c06677f4bfc0c86
Matthew Cavanagh <maelstrome26@gmail.com>
2013-10-19 15:48:07 +0100

    Converted remotely grabbed files into local files for performance boosting. #23

---
+++ includes/includes.php
@@ -0,0 +1,14 @@
+<link href="css/sitewide.css" rel="stylesheet" />
+<link href='http://fonts.googleapis.com/css?family=Noto+Sans:400,700' rel='stylesheet' type='text/css' />
+<?php include("includes/mysqlconnect.php") ?>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" href="css/jquery-ui.css" />
+
+<script type="text/javascript" src="js/jquery-1.9.1.js"></script>
+<script type="text/javascript" src="js/jquery-ui.js"></script>
+<script type="text/javascript" src="js/jquery-ui-timepicker-addon.js"></script>
+<script type="text/javascript" src="js/animatedcollapse.js"></script>
+<script type="text/javascript" src="js/jquery.validate.js"></script>
+<script type="text/javascript" src="js/additional-methods.js"></script>
+
+<script src="http://code.highcharts.com/highcharts.js"></script>
