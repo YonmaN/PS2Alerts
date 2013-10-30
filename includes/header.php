87a1ea18f3360761ca8b0b6039c0a46942df08ec
Matthew Cavanagh <maelstrome26@gmail.com>
2013-10-30 06:35:26 +0000

    Changed header to new stats

--- includes/header.php
+++ includes/header.php
@@ -1,9 +1,9 @@
 <div id="header">
 	<div id="center" style="width: 100%; margin-left: auto; margin-right: auto;">
         <a href="index.php"><img style="display:block;" src="images/alertbanner.png" alt="Courtesy of Kamteix!"/></a>
-        <div id="links" style="text-align: center; margin-top: 7px;">
+        <div id="links">
             <a class="header_links" href="submitresult.php">Submit Result</a>
-			<a class="header_links" href="alertlist.php">Statistics</a>
+			<a class="header_links" href="index.php">Statistics</a>
         </div>
     </div>
 </div>
